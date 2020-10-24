c39a84c790102793eecce34e36b90b5db1fa7a88
seanmcquaid <sean.mcquaid2@verizon.net>
2020-10-24 14:12:07 -0400

    learned about more conditional rendering fun, really like v-show as an option for a pretty common situation

--- src/components/ConditionalRendering.vue
+++ src/components/ConditionalRendering.vue
@@ -1,6 +1,13 @@
 <template>
+  <template v-if="seen">This is meant for multiple elements</template>
   <div>
     <span v-if="seen" v-on:click="toggle">See me?</span>
+    <span v-else-if="!seen">Hello!</span>
+    <span v-else>GTFO</span>
+    <span v-show="seen"
+      >This will render the element in the dom still but will just toggle
+      display</span
+    >
   </div>
 </template>
 
