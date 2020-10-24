76dc1f63fc902623370b13fbed7bffd69aa9491b
seanmcquaid <sean.mcquaid2@verizon.net>
2020-10-24 15:17:01 -0400

    added all prop types from the docs

---
+++ src/components/Component.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    {{ message }}
+  </div>
+  <TodoItem title="Hello" />
+</template>
+
+// $emit('funcName') can be used from child component to be caught by parent
+component to use that func in the parent component, similar to angular
+
+<script>
+import TodoItem from './TodoItem';
+export default {
+  components: { TodoItem },
+  props: {
+    title: String,
+    likes: Number,
+    isPublished: Boolean,
+    commentIds: Array,
+    author: Object,
+    callback: Function,
+    contactsPromise: Promise, // or any other constructor
+  },
+  emits: {
+    'enlarge-text': () => console.log(),
+  },
+};
+</script>
+
+<style></style>
