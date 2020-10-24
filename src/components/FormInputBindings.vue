a3ac0be497c6f79071047c14e14f7ff02f1131ea
seanmcquaid <sean.mcquaid2@verizon.net>
2020-10-24 14:36:28 -0400

    more form input binding info

---
+++ src/components/FormInputBindings.vue
@@ -0,0 +1,32 @@
+<template>
+  <span>Multiline message is:</span>
+  <p style="white-space: pre-line;">{{ message }}</p>
+  <br />
+  <textarea v-model="message" placeholder="add multiple lines"></textarea>
+  <input type="checkbox" id="checkbox" v-model="checked" />
+  <label for="checkbox">{{ checked }}</label>
+  <div id="v-model-multiple-checkboxes">
+    <input type="checkbox" id="jack" value="Jack" v-model="checkedNames" />
+    <label for="jack">Jack</label>
+    <input type="checkbox" id="john" value="John" v-model="checkedNames" />
+    <label for="john">John</label>
+    <input type="checkbox" id="mike" value="Mike" v-model="checkedNames" />
+    <label for="mike">Mike</label>
+    <br />
+    <span>Checked names: {{ checkedNames }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: '',
+      checked: false,
+      checkedNames: [],
+    };
+  },
+};
+</script>
+
+<style></style>
