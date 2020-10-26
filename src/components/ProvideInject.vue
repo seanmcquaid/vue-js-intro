a62a92fe32a90fa47647d7ff6bdd1a3807740fca
seanmcquaid <sean.mcquaid2@verizon.net>
2020-10-25 20:26:10 -0400

    learned about injecting down to a child from a parent component

---
+++ src/components/ProvideInject.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    {{ todos.length }}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      todos: ['Feed a cat', 'buy tickets'],
+    };
+  },
+  provide: {
+    user: ' John Doe',
+  },
+};
+</script>
+
+// could then use inject to take that property and consume it // must return
+object in provide function when working with references to objects to avoid eror
+// be sure to use Vue.computed() to make child components reactive to parent
+component changes
+
+<style></style>
