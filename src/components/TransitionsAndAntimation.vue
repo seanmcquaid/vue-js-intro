42b47d73ffca70ee183a01962bdc62753d3d98d2
seanmcquaid <sean.mcquaid2@verizon.net>
2020-10-25 20:42:05 -0400

    learned about animations in vue

---
+++ src/components/TransitionsAndAntimation.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="demo">
+    <div
+      @mousemove="xCoordinate"
+      :style="{ backgroundColor: `hsl(${x}, 80%, 50%)` }"
+      class="movearea"
+    >
+      <h3>Move your mouse across the screen...</h3>
+      <p>x: {{ x }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      x: 0,
+    };
+  },
+  methods: {
+    xCoordinate(e) {
+      this.x = e.clientX;
+    },
+  },
+};
+</script>
+
+<style>
+.movearea {
+  transition: 0.2s background-color ease;
+}
+</style>
