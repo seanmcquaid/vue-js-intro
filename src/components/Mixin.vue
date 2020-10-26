da2cca09ede7ea51e8445d4bad3a66a0800729cd
seanmcquaid <sean.mcquaid2@verizon.net>
2020-10-25 20:55:17 -0400

    learned about mixins, sort of like a HOC or interface in concept

---
+++ src/components/Mixin.vue
@@ -0,0 +1,56 @@
+<script>
+export default {
+  created() {
+    this.hello();
+  },
+  methods: {
+    hello() {
+      console.log('hello from mixin!');
+    },
+    conflicting() {
+      console.log('from mixin');
+    },
+  },
+};
+
+// const app = Vue.createApp({
+//   mixins: [myMixin],
+//   data() {
+//     return {
+//       message: 'goodbye',
+//       bar: 'def',
+//     };
+//   },
+//   created() {
+//     console.log('component hook called');
+//     console.log(this.$data); // => { message: "goodbye", foo: "abc", bar: "def" }
+//   },
+// methods: {
+//   conflicting() {
+//   console.log('from self')
+// }
+// }
+// });
+
+//
+// => "mixin hook called"
+// => "component hook called"
+
+// The component's options will take priority when there are conflicting keys in these objects:
+
+// const app = Vue.createApp({
+//   myOption: 'hello!'
+// })
+
+// // inject a handler for `myOption` custom option
+// app.mixin({
+//   created() {
+//     const myOption = this.$options.myOption
+//     if (myOption) {
+//       console.log(myOption)
+//     }
+//   }
+// })
+</script>
+
+// example // const app = Vue.createApp({ mixins: [myMixin] })
