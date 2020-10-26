516194dbac6d8a90bc81a18635675b478edd390b
seanmcquaid <sean.mcquaid2@verizon.net>
2020-10-25 21:06:16 -0400

    SIMPLE custom directive example

---
+++ src/components/CustomDirectives.vue
@@ -0,0 +1,18 @@
+<template>
+  <input v-focus />
+</template>
+
+<script>
+export default {
+  directives: {
+    focus: {
+      // directive definition
+      mounted(el) {
+        el.focus();
+      },
+    },
+  },
+};
+</script>
+
+<style></style>
