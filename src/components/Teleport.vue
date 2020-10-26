8c8d798229bb8f9790f8d9dfe3c773fb3f776317
seanmcquaid <sean.mcquaid2@verizon.net>
2020-10-25 21:08:19 -0400

    teleport is so interesting! really like this for full screen modals

---
+++ src/components/Teleport.vue
@@ -0,0 +1,30 @@
+<template>
+  <button @click="modalOpen = true">
+    Open full screen modal! (With teleport!)
+  </button>
+
+  <teleport to="body">
+    <div v-if="modalOpen" class="modal">
+      <div>
+        I'm a teleported modal! (My parent is "body")
+        <button @click="modalOpen = false">
+          Close
+        </button>
+      </div>
+    </div>
+  </teleport>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      modalOpen: false,
+    };
+  },
+};
+</script>
+
+<style></style>
+
+// AWESOME solution for whole screen modals
