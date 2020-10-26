74d98606dbe419b9da9872cdfd601500f4d17d37
seanmcquaid <sean.mcquaid2@verizon.net>
2020-10-25 21:15:53 -0400

    learned about render props

---
+++ src/components/RenderFunctions.vue
@@ -0,0 +1,23 @@
+<script>
+import Vue from 'vue';
+
+export default {
+  render() {
+    const { h } = Vue;
+
+    return h(
+      'h' + this.level, // tag name
+      {}, // props/attributes
+      this.$slots.default() // array of children
+    );
+  },
+  props: {
+    level: {
+      type: Number,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style></style>
