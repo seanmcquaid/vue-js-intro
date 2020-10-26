ae37523a5b7a4b56b5073c5249902bca525743cb
seanmcquaid <sean.mcquaid2@verizon.net>
2020-10-25 21:59:35 -0400

    example of setup for composition api

---
+++ src/components/Composition.vue
@@ -0,0 +1,26 @@
+<script>
+// import { fetchUserRepositories } from '@/api/repositories'
+// import { ref, onMounted, watch, toRefs } from 'vue'
+
+// // in our component
+// setup (props) {
+//   // using `toRefs` to create a Reactive Reference to the `user` property of props
+//   const { user } = toRefs(props)
+
+//   const repositories = ref([])
+//   const getUserRepositories = async () => {
+//     // update `props.user` to `user.value` to access the Reference value
+//     repositories.value = await fetchUserRepositories(user.value)
+//   }
+
+//   onMounted(getUserRepositories)
+
+//   // set a watcher on the Reactive Reference to user prop
+//   watch(user, getUserRepositories)
+
+//   return {
+//     repositories,
+//     getUserRepositories
+//   }
+// }
+</script>
